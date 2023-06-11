aa2e18ae97fbb801a32c1ae20af3869c1c86b4bd
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2023-06-11 16:58:55 -0300

    tablas y estilos componente cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
-
+import './Cart.css'
+import { useNavigate } from 'react-router-dom'
 const Cart = () => {
+    const navigate = useNavigate()
     return (
-        <>
-            <h3>sOy el cart</h3>
-        </>
+        <div className='cart'>
+            <h3 className='cart-title'>Resumen de compra</h3>
+            <table className="cart-table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Hoodie Mint</td>
+                        <td>10</td>
+                        <td>$1100</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Hoodie Mint</td>
+                        <td>10</td>
+                        <td>$1100</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Hoodie Mint</td>
+                        <td>10</td>
+                        <td>$1100</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p className='cart-total'>Total: $3300 </p>
+            <section className='cart-buttons'>
+                <button onClick={()=>navigate("/shop")} className='cart-add-btn'>Add Products</button>
+                <button className='cart-rm-btn'>Remove All Products</button>
+                <button className='cart-buy-btn'>Buy Products</button>
+            </section>
+            
+        </div>
     )
 }
 
