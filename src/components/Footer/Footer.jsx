61f5a83394eea0dd3a8f5b9aa79e48fbd629d17d
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2023-07-06 22:11:21 -0300

    se agrego el link a github en el footer y se elimino la carpeta docs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Footer.css'
-
+import { Link } from 'react-router-dom'
+import {AiFillGithub} from 'react-icons/ai'
 const Footer = () => {
     return (
         <footer>
             <p className='copyright'> © Made by Lucas Zelaya</p>
+            <Link to={'https://github.com/lvcas-z'}><li className='github-icon'><AiFillGithub /></li></Link>
         </footer>
     )
 }
